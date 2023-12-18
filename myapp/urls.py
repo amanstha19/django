9288a0836ec2f36ce6fb25e8f45ef1f940cfd4b8
shresthaaman27@gmail.com <shresthaaman27@gmail.com>
2023-12-18 21:45:51 +0545

    .

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import hello_world, home, portfolio, hello, temp_inherit_home
+from .views import hello_world, home, portfolio, hello, temp_inherit_home,temp_inherit_features,\
+temp_inherit_pricing
 
 urlpatterns =[
     path("hello/", hello),
    # path("", portfolio),
-    path("", home, name='main_home'),
-    path("portfolio/", portfolio, name='portfolio_page'),
-    path("temp-inherit/", temp_inherit_home, name='temp_inherit_home')
+    path("", home, name='home'),
 
+    path("temp-inherit/", temp_inherit_home, name='temp_inherit_home'),
+    path("features/", temp_inherit_features, name='temp_inherit_features'),
+
+    path("pricing/", temp_inherit_pricing, name='temp_inherit_pricing'),
+    path("portfolio/", portfolio, name='main_home')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -18,6 +18,7 @@ def hello_world(request):
     </html>
     
     ''')
+
 def hello(request):
     return HttpResponse("<h1> HELLO </h1>")
 
@@ -26,5 +27,42 @@ def home(request):
     return render(request, template_name='myapp/home.html')
 def portfolio(request):
     return render(request, template_name="myapp/index.html")
-def  temp_inherit_home(request):
-    return render(request, template_name='myapp/temp_inherit_home.html')
+def temp_inherit_home(request):
+
+    items = [
+        {"name": "shirt", "store_location": "KTM", "price": 1000},
+        {"name": "hoodies", "store_location": "BKT", "price": 3000},
+        {"name": "trousers", "store_location": "LTP", "price": 2000},
+
+    ]
+    return render(request, template_name='myapp/temp_inherit_home.html', context={"name": "Aman Shrestha", "age": 21,
+                                                                "address": "Bhaktapur", "items": items})
+def temp_inherit_features(request):
+    items = [
+        {"name": "hock Resistant Portable Bluetooth Speaker",
+         "description": "The design is amazing and feels"
+                        " like premium product. The sound quality is good and supports multiple devices both via Bluetooth and AUX mode. It would have "
+                        "been awesome if the device come of with type c charging as it is widely used by most of the devices these days. But, this does not hold us back to buy this amazing Nepali brand product. I would recommend this product to everyone who are looking for budget friendly, "
+                        "quality Bluetooth speaker"},
+
+
+        {
+            "name": " Office Editing and Gaming Desktop With Intel Core i5 6th Gen 6500 3.6Ghz"
+                    " Processor, 60Hz 19” PS Tech "
+                    "LED HD Monitor, 8GB 3200Mhz"
+                    " RAM",
+            "description": "Processor: Intel core i5 6500 3.6Ghz Turbo Quad Core Skylake LGA 1151 6MB Cache 6th Gen Processor With 4 Logical Cores and 4 Threads "
+                           "(3 Yrs. Warranty)"
+"Board: Esonic H110 Dual RAM Channel Pcie Board (1 Year Warranty)"
+"RAM: Crucial Best Brand 8GB 3200Mhz RAM(Lifetime Brand Warranty on Crucial)"
+"SSD: Kingston SATA 256GB Branded SSD (1 Year Warranty)"
+        }
+
+
+    ]
+    return render(request, template_name='myapp/temp_inherit_features.html', context={"name": "Aman Shrestha", "age": 21,
+                                                                "address": "Bhaktapur", "items": items})
+
+def temp_inherit_pricing(request):
+    return render(request, template_name='myapp/temp_inherit_pricing.html')
+
