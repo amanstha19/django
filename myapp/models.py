ec26ee7e94a4b6a2fb7e26df1cee24d785a4b3cb
shresthaaman27@gmail.com <shresthaaman27@gmail.com>
2023-12-23 19:13:19 +0545

    ..

--- myapp/models.py
+++ myapp/models.py
@@ -9,5 +9,5 @@ class Student(models.Model):
     age = models.IntegerField()
     address = models.CharField(max_length=50)
     email = models.EmailField(max_length=20)
-    classroom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, related_name="classroom_students")
+    classroom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, related_name="classroom_students", null=True, blank=True)
 
